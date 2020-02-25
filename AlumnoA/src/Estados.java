7a6744ffba50c5c7245b0c7feb25a6cecf1acd63
DavidT <David@gmail.com>
2020-02-25 20:23:29 +0100

    Alumno A

---
+++ AlumnoA/src/Estados.java
@@ -0,0 +1,4 @@
+public enum Estados 
+{
+		Aprovada, Pendiente, Cobrada
+}
---
+++ AlumnoA/src/IFactura.java
@@ -0,0 +1,17 @@
+public interface IFactura 
+{
+	public void CambiarEstado()
+	{
+		
+	}
+	
+	public float getTotal()
+	{
+		
+	}
+	
+	public float getCantidadIva()
+	{
+		
+	}
+}
